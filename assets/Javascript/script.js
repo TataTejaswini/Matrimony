af1a2a7726c8215ca13d91d1f42f382855d110a7
TataTejaswini <tejaswinitata@gmail.com>
2026-05-19 11:41:09 +0530

    updated project files

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -176,3 +176,31 @@ function exploreNow() {
             event.target.reset();
         }, 1500);
     }
+
+    // COUNTER ANIMATION
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (current < target) {
+            counter.innerText = `${Math.ceil(current + increment)}`;
+            setTimeout(updateCounter, 20);
+        } else {
+            // ADD + OR %
+            if (target === 97) {
+                counter.innerText = target + "%";
+            } else {
+                counter.innerText = target + "+";
+            }
+        }
+    };
+
+    updateCounter();
+});
